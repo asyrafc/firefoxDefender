d81479c1bd04f48e76d191a2a8a386f8614abf22
ghostbyte <asyrafcloud@gmail.com>
2017-09-27 09:50:58 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+	<?php
+		$name = $_GET['name'];
+		echo "Welcome $name<br>";
+		echo "<a href=\"javascript:alert(1)\">Click to Download</a>";
+	?>
+	<!--Test Home
+	<video controls> <source src="WSP-115.mp4" type="video/mp4"> </video>
+	<form method = "post" action = "validate.php">
+		User : <input type = "text" name = "user"/>
+		Pass : <input type = "password" name = "pass"/>
+		<input type = "submit" name = "submitted" value = "Login"/>
+	</form>-->
+</html>
+
